2df4276dd18ba565c31a8b5cc9fd5bb76ac1ee3b
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-29 13:40:29 +0200

    feat:done

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const button = document.querySelector("button");
-const title = document.querySelector("title");
 const body = document.querySelector("body");
 
 button.addEventListener("click", buttonClick);
 
 function buttonClick() {
-  if ((title.innerText = "Good Morning!")) {
-    title.innerText = "Good Night!";
-  } else if ((title.innerText = "Good Night!")) {
-    title.innerText = "Good Morning!";
+  if (document.title === "Good Morning!") {
+    document.title = "Good Night!";
+  } else {
+    document.title = "Good Morning!";
   }
   button.classList.toggle("button-dark");
   body.classList.toggle("body-dark");
